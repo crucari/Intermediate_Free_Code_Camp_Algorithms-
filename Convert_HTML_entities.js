935ab4300cd961678b8780ca56aa1eec4b6e8926
Carina Cruz <lecruz@bu.edu>
2018-05-22 23:50:07 +0000

    Coverting HTML elements

---
+++ Convert_HTML_entities.js
@@ -0,0 +1,33 @@
+function convertHTML(str) {
+  // Split by character
+
+  var temp = str.split('');
+
+
+
+  for (var i = 0; i < temp.length; i++) {
+    switch (temp[i]) {
+      case '<':
+        temp[i] = '&lt;';
+        break;
+      case '&':
+        temp[i] = '&amp;';
+        break;
+      case '>':
+        temp[i] = '&gt;';
+        break;
+      case '"':
+        temp[i] = '&quot;';
+        break;
+      case "'":
+        temp[i] = "&apos;";
+        break;
+    }
+  }
+
+  temp = temp.join('');
+  return temp;
+}
+
+//test here
+convertHTML("Dolce & Gabbana");
