541a2a6a25a1a33487db5625a3980282d5a959f8
Carina Cruz <lecruz@bu.edu>
2018-05-23 01:32:30 +0000

    update

--- Arguments_Optional.js
+++ Arguments_Optional.js
@@ -142,6 +142,28 @@ function addTogether() {
 
 addTogether(2,3);
 
+___
+
+
+function addTogether() {
+
+    var numOne = arguments[0];
+    function addTwo(numTwo) {
+        return typeof(numTwo) === 'number' ? numOne + numTwo : undefined;
+    }
+    return typeof(numOne) !== 'number' ? undefined : (arguments.length === 2 ? addTwo(arguments[1]) : addTwo);
+}
+
+addTogether(2,3);
+
+// if 2 arguments add them together
+// must be numbers
+// if 1 argument send function back to get the second item
+
+// if 2 arguments add them together
+// must be numbers
+// if 1 argument send function back to get the second item
+
 // if 2 arguments add them together
 // must be numbers
 // if 1 argument send function back to get the second item
