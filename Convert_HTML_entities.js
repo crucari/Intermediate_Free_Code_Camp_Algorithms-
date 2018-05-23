3146f65ea6379854cd91005726d2c10ffcd8eabd
Carina Cruz <lecruz@bu.edu>
2018-05-23 00:46:22 +0000

    update

--- Convert_HTML_entities.js
+++ Convert_HTML_entities.js
@@ -31,3 +31,60 @@ function convertHTML(str) {
 
 //test here
 convertHTML("Dolce & Gabbana");
+
+______________
+
+//converting &, <, >, " to &amp, &It, &qt, &quot, &apos
+// 1) Split characters
+// 2) hashmap  ->
+// 3) join
+var hashMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&apos;'
+};
+
+function convertHTML(str) {
+    var strArray = str.split('');
+    strArray = strArray.map(function(char) {
+        if (hashMap[char]) {
+            return hashMap[char];
+        }
+        return char;
+    });
+    str = strArray.join('');
+    return str;
+}
+
+convertHTML("Dolce & Gabbana");
+_________
+
+var hashMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&apos;'
+};
+
+function convertHTML(str) {
+    var strArray = str.split('');
+    strArray = strArray.map(function(char) {
+        return hashMap[char] ? hashMap[char] : char;
+    });
+    str = strArray.join('');
+    return str;
+}
+
+convertHTML("Dolce & Gabbana");
+
+______________
+
+var hashMap = {'&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&apos;'};
+function convertHTML(str) {
+    return str.split('').map(function(char) {return hashMap[char] ? hashMap[char] : char;}).join('');
+}
+
+convertHTML("Dolce & Gabbana");
